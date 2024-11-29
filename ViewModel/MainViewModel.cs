2eee52f29e1a7330da71abfaaf173a70253c58f4
pnson1322 <pnson22@gmail.com>
2024-11-29 13:23:51 +0700

    change

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Windows;
+using FontAwesome.Sharp;
+using System.Windows.Input;
 using QuanLyBenhVien.Model;
 using QuanLyBenhVien.Repositories;
 
@@ -15,27 +17,77 @@ namespace QuanLyBenhVien.ViewModel
         private UserAccountModel _currentUserAccount;
         private IUserRepository _userRepository;
 
+        private ViewModelBase _currentChildView;
+        private string _caption;
+        private IconChar _icon;
+
         internal UserAccountModel CurrentUserAccount { get => _currentUserAccount; set { _currentUserAccount = value; OnPropertyChanged(nameof(CurrentUserAccount)); } }
+
+        public ViewModelBase CurrentChildView
+        {
+            get => _currentChildView;
+            set
+            {
+                _currentChildView = value;
+                OnPropertyChanged(nameof(CurrentChildView));
+            }
+        }
+        public string Caption
+        {
+            get => _caption;
+            set
+            {
+                _caption = value;
+                OnPropertyChanged(nameof(Caption));
+            }
+        }
+        public IconChar Icon
+        {
+            get => _icon;
+            set
+            {
+                _icon = value;
+                OnPropertyChanged(nameof(Icon));
+            }
+        }
+
         public MainViewModel()
         {
             _userRepository = new UserRepository();
             CurrentUserAccount = new UserAccountModel();
+
+            //Initialize commands
+            ShowBenhNhanViewCommand = new ViewModelCommand(ExecuteShowBenhNhanViewCommand);
+
+            //Default view
+            ExecuteShowBenhNhanViewCommand(null);
+
             LoadCurrentUserData();
         }
+
+        private void ExecuteShowBenhNhanViewCommand(object obj)
+        {
+            CurrentChildView = new BenhNhanViewModel();
+            Caption = "Bệnh nhân";
+            Icon = IconChar.BedPulse;
+        }
+
+        // commands
+        public ICommand ShowBenhNhanViewCommand { get; }
 
         private void LoadCurrentUserData()
         {
             //var user = _userRepository.GetByID("abcxyz");
             var user = _userRepository.GetByID(Thread.CurrentPrincipal.Identity.Name);
-            if (user != null) 
+            if (user != null)
             {
                 CurrentUserAccount.UserID = user.Id;
-                CurrentUserAccount.DisplayName = $"Welcome {user.LastName} {user.FirstName} ;)";
+                CurrentUserAccount.DisplayName = $"{user.LastName} {user.FirstName}";
                 CurrentUserAccount.ProfilePicture = null;
             }
             else
             {
-                CurrentUserAccount.DisplayName= "Đăng nhập không hợp lệ!!!";
+                CurrentUserAccount.DisplayName = "Đăng nhập không hợp lệ!!!";
                 //Application.Current.Shutdown();
             }
         }
